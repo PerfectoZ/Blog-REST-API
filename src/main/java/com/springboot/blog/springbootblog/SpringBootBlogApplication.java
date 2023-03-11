25c55dc2e199288fc6b504029dc4835be7f5e9a2
Mandeep Taneja <mandeeptaneja21@outlook.com>
2023-03-11 18:23:45 +0530

    Dockerize the application in /src/main/docker

--- src/main/java/com/springboot/blog/springbootblog/SpringBootBlogApplication.java
+++ src/main/java/com/springboot/blog/springbootblog/SpringBootBlogApplication.java
@@ -1,14 +1,20 @@
 package com.springboot.blog.springbootblog;
 
 import com.springboot.blog.springbootblog.Entity.Role;
+import com.springboot.blog.springbootblog.Entity.User;
 import com.springboot.blog.springbootblog.Repository.RoleRepository;
+import com.springboot.blog.springbootblog.Repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringBootBlogApplication implements CommandLineRunner {
@@ -20,18 +26,45 @@ public class SpringBootBlogApplication implements CommandLineRunner {
 		SpringApplication.run(SpringBootBlogApplication.class, args);
 	}
 
+	@Autowired
+	RoleRepository roleRepository;
 
 	@Autowired
-	private RoleRepository roleRepository;
+	UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	@Override
 	public void run(String... args) throws Exception {
-		Role adminRole = new Role();
-		adminRole.setName("ROLE_ADMIN");
-		roleRepository.save(adminRole);
+		try {
+			Role adminRole = new Role();
+			adminRole.setName("ROLE_ADMIN");
+			roleRepository.save(adminRole);
+		}
+		catch(Exception e) {
 
-		Role userRole = new Role();
-		userRole.setName("ROLE_USER");
-		roleRepository.save(userRole);
+		}
+		try {
+			Role userRole = new Role();
+			userRole.setName("ROLE_USER");
+			roleRepository.save(userRole);
+		}
+		catch(Exception e) {
+
+		}
+		try {
+			User admin = new User();
+			admin.setEmail("admin@gmail.com");
+			admin.setName("admin");
+			admin.setUsername("admin");
+			admin.setPassword(passwordEncoder.encode("admin"));
+			Set<Role> roles = new HashSet<>();
+			Role userRole = roleRepository.findRoleByName("ROLE_ADMIN").get();
+			userRepository.save(admin);
+		}
+		catch(Exception e) {
+
+		}
 	}
 }
