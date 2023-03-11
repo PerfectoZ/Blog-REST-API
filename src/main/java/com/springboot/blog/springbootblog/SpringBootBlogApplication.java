911efd75a29f98b4a90ec807e2cead638be9b22b
Mandeep Taneja <mandeeptaneja21@outlook.com>
2023-03-11 15:52:43 +0530

    Ready for deployment ?

--- src/main/java/com/springboot/blog/springbootblog/SpringBootBlogApplication.java
+++ src/main/java/com/springboot/blog/springbootblog/SpringBootBlogApplication.java
@@ -1,13 +1,17 @@
 package com.springboot.blog.springbootblog;
 
+import com.springboot.blog.springbootblog.Entity.Role;
+import com.springboot.blog.springbootblog.Repository.RoleRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-public class SpringBootBlogApplication {
+public class SpringBootBlogApplication implements CommandLineRunner {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
@@ -16,4 +20,18 @@ public class SpringBootBlogApplication {
 		SpringApplication.run(SpringBootBlogApplication.class, args);
 	}
 
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
